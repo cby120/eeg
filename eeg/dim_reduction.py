0acfb7c5fb658bccdfdd9d647615137594fac336
Huang Yanbin <1219382437@qq.com>
2024-06-17 00:03:05 +0800

    commit test

--- eeg/dim_reduction.py
+++ eeg/dim_reduction.py
@@ -1,8 +1,10 @@
 import numpy as np
 from sklearn.decomposition import PCA
 
-pca = PCA(n_components=10)
-pca.fit()
-pca.transform()
+# pca = PCA(n_components=10)
+# pca.fit()
+# pca.transform()
+
+print("hello world!")
 
 
