889c5d47897d7cae1a03960a85ac8b1b8b8be8d8
kiriasuna19 <94943156+kiriasuna19@users.noreply.github.com>
2024-06-17 17:10:55 +0800

    Update model.py

--- eeg/model.py
+++ eeg/model.py
@@ -1,1 +1,72 @@
+import torch
+from torch import nn
+from torch.nn import functional
+
+class Conv2dWithConstraint(nn.Conv2d):
+    def __init__(self, *args, max_norm=1, **kwargs):
+        self.max_norm = max_norm
+        super(Conv2dWithConstraint, self).__init__(*args, **kwargs)
+
+    def forward(self, x):
+        self.weight.data = torch.renorm(
+            self.weight.data, p=2, dim=0, maxnorm=self.max_norm
+        )
+        return super(Conv2dWithConstraint, self).forward(x)
+
+class EEGNetReproduce(nn.Module):
+    def __init__(self, n_channels, n_classes, input_window_size,
+                 F1=8, D=2, F2=16, kernel_length=64, drop_p=0.25):
+        super(EEGNetReproduce, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.F1 = F1
+        self.D = D
+        self.F2 = F2
+        self.kernel_length = kernel_length
+        self.drop_p = drop_p
+        self.input_windows_size = input_window_size
+        self.block_temporal_conv = nn.Sequential(
+            nn.Conv2d(1, self.F1, (1, self.kernel_length),
+                      stride=1, bias=False, padding='same'),
+            nn.BatchNorm2d(self.F1)
+        )
+        self.block_spacial_conv = nn.Sequential(
+            Conv2dWithConstraint(self.F1, self.F1 * self.D, (self.n_channels, 1),
+                                 max_norm=1, stride=1, bias=False, groups=self.F1, padding=(0, 0)),
+            nn.BatchNorm2d(self.F1 * self.D),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=(1, 4), stride=(1, 4)),
+            nn.Dropout(p=self.drop_p)
+        )
+        self.block_separable_conv = nn.Sequential(
+            nn.Conv2d(self.F1 * self.D, self.F1 * self.D, (1, 16),
+                      stride=1, bias=False, groups=self.F1 * self.D, padding='same'),
+            nn.Conv2d(self.F1 * self.D, self.F2, (1, 1),
+                      stride=1, bias=False, padding=(0, 0)),
+            nn.BatchNorm2d(self.F2),
+            nn.ELU(),
+            nn.AvgPool2d(kernel_size=(1, 8), stride=(1, 8)),
+            nn.Dropout(p=self.drop_p),
+            nn.Flatten()
+        )
+        block_conv = nn.Sequential(
+            self.block_temporal_conv,
+            self.block_spacial_conv,
+            self.block_separable_conv
+        )
+        out = block_conv(torch.ones((1, 1, self.n_channels, self.input_windows_size), dtype=torch.float32))
+        self.block_classifier = nn.Sequential(
+            nn.Linear(out.cpu().data.numpy().shape[1], self.n_classes),
+            # nn.LogSoftmax(dim=1)
+        )
+
+    def forward(self, x):
+        while len(x.shape) < 4:
+            x = x.unsqueeze(-1)
+        x = x.permute(0, 3, 1, 2)
+        x = self.block_temporal_conv(x)
+        x = self.block_spacial_conv(x)
+        x = self.block_separable_conv(x)
+        x = self.block_classifier(x)
+        return x
 
