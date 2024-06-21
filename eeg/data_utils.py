e67b40c1f33424e2715d62227f5c9c4d12c6508a
kiriasuna19 <94943156+kiriasuna19@users.noreply.github.com>
2024-06-21 15:36:29 +0800

    Update data_utils.py

--- eeg/data_utils.py
+++ eeg/data_utils.py
@@ -2,10 +2,10 @@ import os
 import torch
 import random
 import numpy as np
+from typing import List
+from torch.utils.data import Dataset, RandomSampler, Subset, DataLoader
+import pickle
 from typing import List, Union, Callable, Any, Iterable
-from torch.utils.data import Dataset, RandomSampler, Subset
-from torch.utils import data
-
 N_TASKS = 14
 N_SUBJECTS = 109
 DURATION = 6.25e-3
@@ -69,10 +69,9 @@ class EEGDatasetHYB(Dataset):
                         label = int(event.split('_')[0])
                         self._meta_data.append((os.path.join(path_dir, event), label))
 
-
+#how to split train and val
 class EEGDataset(Dataset):
     p = 0.9  # partition ratio of train and test set from full dataset
-
     def __init__(
         self,
         data_dir: str,
@@ -100,11 +99,16 @@ class EEGDataset(Dataset):
                 raise ValueError("transforms must be Callable or Iterable of Callable")
         else:
             self.transforms = None
-        self._meta_data = []
-
+        pkl_name = '2_t_600_remove_some_people.pkl'
         # load data based on different attribute
-
-        self.load_func(subjects, tasks)
+        if os.path.exists(pkl_name):
+            with open(pkl_name, 'rb') as f:
+                self._meta_data = pickle.load(f)
+        else:
+            self._meta_data = []
+            self.load_func(subjects, tasks)
+            with open(pkl_name, 'wb') as f:
+                pickle.dump(self._meta_data, f)
 
         if shuffle:
             random.shuffle(self._meta_data)
@@ -143,9 +147,16 @@ class EEGDataset(Dataset):
             for task in tasks:
                 for event in os.listdir(os.path.join(subj_pth, task)):
                     label = int(event.split('_')[0])
-                    self._meta_data.append((os.path.join(self._data_dir, subject, task, event), label))
+                    #print(label)
+                    if label !=0 and label !=3 and label !=4:
+                        meta_path = os.path.join(self._data_dir, subject, task, event)
+                        data = np.load(meta_path)["data"]
+                        t,c = data.shape 
+                        if t >= 600: #and label!= 0:
+                            self._meta_data.append((meta_path, label))
+                            #print(self._meta_data)
 
-
+                    
 def get_random_subset(dataset: Dataset, n_samples: int):
     """
     Random Sampled dataset subset
@@ -158,20 +169,22 @@ def get_random_subset(dataset: Dataset, n_samples: int):
     return Subset(dataset, sample_ind)
 
 
+#两个collect——fn
 def trim_last(batch):
-    """
+    """_summary_
     collate_fn for dataloader
-    :param batch: List[Tuple[x, y], ...]
-    :return: batched data
+    :param batch: _description_
+    :type batch: _type_
+    :return: _description_
+    :rtype: _type_
     """
     x_list, y_list = tuple(zip(*batch))
     x_list: List[torch.Tensor]  # [T, C]
     len_list = np.array(list(_.size(0) for _ in x_list))
-    min_len = len_list.min()
+    min_len = 100 #len_list.min()#
     x_list_ = [_[:min_len] for _ in x_list]
     return torch.stack(x_list_), torch.stack(y_list)
-
-
+    
 def trim_symm(batch):
     """
     collate_fn for dataloader
@@ -183,21 +196,45 @@ def trim_symm(batch):
     # print(x_list[0].shape)
     len_list = np.array(list(_.size(0) for _ in x_list))
     # print(len_list)
-    min_len = len_list.min()
+    min_len = 600 #len_list.min()
     dl = len_list - min_len
     ltil = dl // 2  # list_trim_index_left
     ltir = len_list - dl + ltil
     x_list_ = [_[til: tir] for _, til, tir in zip(x_list, ltil, ltir)]
     return torch.stack(x_list_), torch.stack(y_list)
 
+# def trim_symm_3d(batch):
+#     """
+#     collate_fn for dataloader
+#     :param batch: List[Tuple[x, y], ...]
+#     :return: batched data
+#     """
+#     x_list, y_list = tuple(zip(*batch))
+#     x_list: List[torch.Tensor]  # [T, C, F]
+#     # print(x_list[0].shape)
+#     len_list = np.array(list(_.size(0) for _ in x_list))
+#     # print(len_list)
+#     min_len = 600 #len_list.min()
+#     dl = len_list - min_len
+#     ltil = dl // 2  # list_trim_index_left
+#     ltir = len_list - dl + ltil
+#     x_list_ = [_[til: tir] for _, til, tir in zip(x_list, ltil, ltir)]
+#     return torch.stack(x_list_), torch.stack(y_list)
+
+
 
 if __name__ == "__main__":
     # dataset = EEGDatasetHYB('../data', 'full')
     # dataset = EEGDatasetHYB('../../data', 'subject', subjects=[1])
     # dataset = EEGDatasetHYB('../../data', 'task', subjects=[1], tasks=[3,4])
-    dataset = EEGDataset("../data", subjects=[1, 2, 3], tasks=[2, 3, 4])
-    # print(len(dataset))
-    
-    # dl = DataLoader(dataset, 10, collate_fn=trim_symm)
-    dl = data.DataLoader(dataset, 10, collate_fn=trim_last)
-    print(next(iter(dl)))
+    # dataset = EEGDataset("./data", tasks=[3,4,5,6,7,8,9,10,11,12,13,14],subjects= [x for x in range(1, 110) if x not in [88, 90, 92, 100, 104, 106]])
+    #print(len(dataset)) 
+    #35612 35414
+    train_dataset = EEGDataset(data_dir="./data", tasks=[3,4,5,6,7,8,9,10,11,12,13,14],subjects= [x for x in range(1, 110) if x not in [88, 90, 92, 100, 104, 106]],shuffle=False, train=True)
+
+    # from braindecode.datasets import MOABBDataset
+    # from braindecode.preprocessing.windowers import create_windows_from_events
+    # from braindecode.preprocessing.preprocess import (preprocess, Preprocessor)
+    # # # dl = DataLoader(dataset, 10, collate_fn=trim_symm)
+    # dl = DataLoader(dataset, 10, collate_fn=trim_last)
+    # print(next(iter(dl)))
